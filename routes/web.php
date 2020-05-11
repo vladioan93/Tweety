1b1d557560b08cce37b12e8609426ee4010e5171
vlad <prioteasa.vlad@gmail.com>
2020-05-11 17:45:07 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/tweets', 'TweetController@index')->name('home');
+    Route::post('/tweets', 'TweetController@store');
+
+    Route::post('/tweets/{tweet}/like', 'TweetLikesController@store');
+    Route::delete('/tweets/{tweet}/like', 'TweetLikesController@destroy');
+
+    Route::post(
+        '/profiles/{user:username}/follow',
+        'FollowsController@store'
+    )->name('follow');
+
+    Route::get(
+        '/profiles/{user:username}/edit',
+        'ProfilesController@edit'
+    )->middleware('can:edit,user');
+
+    Route::patch(
+        '/profiles/{user:username}',
+        'ProfilesController@update'
+    )->middleware('can:edit,user');;
+
+    Route::get('/explore', 'ExploreController');
+});
+
+Route::get('/profiles/{user:username}', 'ProfilesController@show')->name(
+    'profile'
+);
+
+
+
+Auth::routes();
